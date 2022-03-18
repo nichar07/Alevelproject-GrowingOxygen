43769fe59e6edfb668821a490e928cb314c5f0d2
nichar0708 <nichar0708@highgateschool.org.uk>
2022-03-18 12:21:04 +0000

    me when I github

--- Calculations_Code/Database.py
+++ Calculations_Code/Database.py
@@ -3,11 +3,11 @@ from GUI_Code.PlantObjects import Plant as P
 
 
 PlantList = [
-            P('Devils Ivy', 22, 18, 3, 2, 8,'Water once a week and mist',[0,3], r'C:\Users\nicpa\PycharmProjects\Alevelproject-GrowingOxygen-mk1\PlantImg\devils ivy.PNG'),
-            P('Peace Lily', 24, 18, 4, 1, 7,'Water when dry and clean leaves',[4,1,3], r'C:\Users\nicpa\PycharmProjects\Alevelproject-GrowingOxygen-mk1\PlantImg\Peace Lily.png'),
-            P('Snake Plant', 26, 18, 1, 2, 10, 'Water sparingly',[],r'C:\Users\nicpa\PycharmProjects\Alevelproject-GrowingOxygen-mk1\PlantImg\snake plant.png'),
-            P('Parlour Palm', 26, 20, 2, 3, 8, 'Try not to avoid overwatering and repot once a year',[3],r'C:\Users\nicpa\PycharmProjects\Alevelproject-GrowingOxygen-mk1\PlantImg\parlour Palm.png'),
-            P('Caladium',22,18,5,2,3,'Water barely and mist regularly',[1],r'C:\Users\nicpa\PycharmProjects\Alevelproject-GrowingOxygen-mk1\PlantImg\Caladium.png'),
-            P('Orchid',24,18,4,1,3,'Water never and mist often',[0,3],r'C:\Users\nicpa\PycharmProjects\Alevelproject-GrowingOxygen-mk1\PlantImg\Orchid.png')
+            P('Devils Ivy', 22, 18, 3, 2, 8,'Water once a week and mist',[0,3], 'devils ivy.PNG'),
+            P('Peace Lily', 24, 18, 4, 1, 7,'Water when dry and clean leaves',[4,1,3], 'Peace Lily.png'),
+            P('Snake Plant', 26, 18, 1, 2, 10, 'Water sparingly',[],'snake plant.png'),
+            P('Parlour Palm', 26, 20, 2, 3, 8, 'Try not to avoid overwatering and repot once a year',[3],'parlour Palm.png'),
+            P('Caladium',22,18,5,2,3,'Water barely and mist regularly',[1],'Caladium.png'),
+            P('Orchid',24,18,4,1,3,'Water never and mist often',[0,3],'Orchid.png')
 ]
 
